88d5950c9db4d682950c878ad09dc259ac9f4708
Atika1337 <Yandraxtr@gmail.com>
2026-02-26 12:35:46 +0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+# with open("test.txt", "w", encoding="utf-8") as file:
+#     file.write("Привет, файл!")
+
+
+
+
+
+
+
+
+
+
+
+# with open("user_text.txt", "w", encoding="utf-8") as file:   
+#     file.write(input())
+# with open("user_text.txt", "r", encoding="utf-8") as file:    
+#     if len("user_text.txt") >= 5:
+#         print("user_text.txt")
